3e38b3879658821eb6c8de8538166591d8d2ce4f
BrendaDeSantanaLima <brenda3606b@gmail.com>
2026-01-23 16:58:56 -0300

    Autenticação CRUD periodo e presenca

--- backend/src/routes/periodo.routes.js
+++ backend/src/routes/periodo.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/authMiddleware');
+const authorize = require('../middlewares/authorizeMiddleware');
 const periodoController = require('../controllers/periodo.controller');
 
-router.post('/', periodoController.createPeriodo);
-router.get('/', periodoController.getPeriodos);
-router.get('/:id', periodoController.getPeriodoById);
-router.put('/:id', periodoController.updatePeriodo);
-router.put('/inativar/:id', periodoController.inativarPeriodo);
+
+router.post('/',  authorize(['admin', 'secretaria']),periodoController.createPeriodo);
+router.get('/', authorize(['admin', 'secretaria']),periodoController.getPeriodos);
+router.get('/:id', authorize(['admin', 'secretaria']),periodoController.getPeriodoById);
+router.put('/:id', authorize(['admin', 'secretaria']),periodoController.updatePeriodo);
+router.put('/inativar/:id', authorize(['admin', 'secretaria']),periodoController.inativarPeriodo);
 
 module.exports = router;
--- backend/src/routes/presenca.routes.js
+++ backend/src/routes/presenca.routes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/authMiddleware');
+const authorize = require('../middlewares/authorizeMiddleware');
 const presencaController = require('../controllers/presenca.controller');
 
-router.get('/turma/:id_turma/aula', presencaController.getPresencasPorAula);
-router.get('/turma/:id_turma', presencaController.getPresencasPorTurma);
 
-router.post('/', presencaController.createPresenca);
-router.get('/', presencaController.getAllPresencas);
-router.get('/:id', presencaController.getPresencaById);
-router.put('/:id', presencaController.updatePresenca);
+router.get('/turma/:id_turma/aula',auth, authorize(['admin', 'secretaria', 'treinador']), presencaController.getPresencasPorAula);
+router.get('/turma/:id_turma',auth, authorize(['admin', 'secretaria', 'treinador']), presencaController.getPresencasPorTurma);
+
+router.post('/', authorize(['admin', 'secretaria', 'treinador']),presencaController.createPresenca);
+router.get('/', authorize(['admin', 'secretaria', 'treinador']),presencaController.getAllPresencas);
+router.get('/:id',authorize(['admin', 'secretaria', 'treinador']),presencaController.getPresencaById);
+router.put('/:id', authorize(['admin', 'secretaria', 'treinador']),presencaController.updatePresenca);
 
 
 module.exports = router;
